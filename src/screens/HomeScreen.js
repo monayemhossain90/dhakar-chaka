4b080fb4e956e0cfc9e8ffc5462edc66445fa001
monayemhossain90 <monayemhossain1990@gmail.com>
2023-05-24 00:06:51 +0600

    upto map marker

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,15 @@
-import { Text, View,Dimensions,StyleSheet, ScrollView  } from "react-native";
+import { Text, View,Dimensions,StyleSheet, ScrollView, Image, FlatList,Item  } from "react-native";
 const SCREEN_WIDTH = Dimensions.get('window').width;
 import {colors,parameters} from "../global/styles";
 import { Icon } from "react-native-elements";
 import { StatusBar } from "expo-status-bar";
+import { filterData } from './../global/data';
 
 const HomeScreen = () => {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
+                {/* menu bar icon */}
             <View style = {styles.icon1}>
                 <Icon 
                 type="material-community"
@@ -18,18 +20,90 @@ const HomeScreen = () => {
             </View>
             </View>
             <ScrollView bounces={false}>
+                {/* banner section */}
                 <View style={styles.home}>
-                    <Text style={styles.text1}>Desress your commute</Text>
+                    <Text style={styles.text1}>Destress your commute</Text>
                     <View style={styles.view1}>
                         <View style={styles.view8}>
                             <Text style={styles.text2}> Read a book.  take a nap. stare out the window</Text>
                             <View style={styles.button1}>
-                                 <Text style={styles.text2}>Ride with uber</Text>
+                                 <Text style={styles.button1Text}>Ride with uber</Text>
                                  </View>
                         </View>
+                        <View>
+                            <Image
+                            style={styles.image1}
+                            source ={require('../../assets/uberCar.png')}
 
+                            />
+                        </View>
                     </View>
+                   
                 </View>
+                {/* item section */}
+                <View>
+                        <FlatList
+                        numRows = {4}
+                        horizontal ={true}
+                        showsHorizontalScrollIndicator={false}
+                        data ={filterData}
+                        keyExtractor={(item)=>item.id}
+                        renderItem={({item})=>(
+                            <View style ={styles.card}> 
+                            <View style = {styles.view2}>
+                                <Image source={item.image} style={styles.image2}/>
+                            </View>
+
+                            <View>
+                                <Text style={styles.title}> {item.name}</Text>
+                            </View>
+
+                            </View>
+                        )}
+
+                        />
+                    </View>
+
+                    {/* where to section */}
+                    <View style={styles.view3}>
+                        <Text style={styles.text3}> Where To ?</Text>
+                          <View style={styles.view4}> 
+                            <Icon
+                            type="material-community"
+                            name ="clock-time-four"
+                            color={colors.grey1}
+                            size={26}
+                            />
+                            <Text style={{marginLeft:5}}>Now</Text>
+                            <Icon
+                            type="material-community"
+                            name ="chevron-down"
+                            color={colors.grey1}
+                            size={26}
+                            />
+                            </View>      
+                    </View>
+                    {/* location */}
+                    <View style={styles.view5}> 
+                        <View style={styles.view6}>
+                            <View style={styles.view7}>
+                            <Icon
+                            type="material-community"
+                            name ="map-marker"
+                            color={colors.black}
+                            size={22}
+                            />
+                            </View>
+                            <View>
+                            <Text style={{fontSize:18,color:colors.black}}> 32 Olivia Road</Text>  
+                              <Text style={{color:colors.grey3}}> kiptown 63, Hamburg</Text>
+                            </View>
+                             
+                        </View>
+                        <View>
+                            
+                        </View>
+                    </View>
             </ScrollView>
             <StatusBar style="light"backgroundColor="#2058c0" translucent={true}/>
         </View>
@@ -52,16 +126,16 @@ const styles = StyleSheet.create({
      
     },
     
-    // image1:{
+    image1:{
      
-    //   height:100,
-    //   width:100,
+      height:100,
+      width:100,
     
-    // },
+    },
     
-    // image2:{height:60,width:60,
-    //         borderRadius:30,
-    //       },
+    image2:{height:60,width:60,
+            borderRadius:30,
+          },
     
     home:{
      backgroundColor:colors.blue,
@@ -103,85 +177,85 @@ const styles = StyleSheet.create({
      marginTop:-2
     
     },
-    // card:{
-    //  alignItems:"center",
-    //  margin:SCREEN_WIDTH/22
+    card:{
+     alignItems:"center",
+     margin:SCREEN_WIDTH/22
     
-    // },
+    },
     
-    // view2:{marginBottom:5,
-    //       borderRadius:15,
-    //       backgroundColor:colors.grey6
-    //     },
+    view2:{marginBottom:5,
+          borderRadius:15,
+          backgroundColor:colors.grey6
+        },
     
-    //     title:{
-    //       color:colors.black,
-    //       fontSize:16
-    //     },
-    // view3:{flexDirection:"row",
-    //          marginTop :5,
-    //          height:50,
-    //          backgroundColor:colors.grey6,
-    //          alignItems:"center",
-    //          justifyContent:"space-between",
-    //         marginHorizontal:15
+        title:{
+          color:colors.black,
+          fontSize:16
+        },
+    view3:{flexDirection:"row",
+             marginTop :5,
+             height:50,
+             backgroundColor:colors.grey6,
+             alignItems:"center",
+             justifyContent:"space-between",
+            marginHorizontal:15
             
-    //          },
+             },
     text3:{marginLeft:15,
             fontSize:20,
             color:colors.black
       },
     
-    // view4:{ flexDirection:"row",
-    //         alignItems:"center",
-    //         marginRight:15,
-    //         backgroundColor:"white",
-    //         paddingHorizontal:10,
-    //         paddingVertical:2,
-    //         borderRadius:20
-    //         },
+    view4:{ flexDirection:"row",
+            alignItems:"center",
+            marginRight:15,
+            backgroundColor:"white",
+            paddingHorizontal:10,
+            paddingVertical:2,
+            borderRadius:20
+            },
     
-    // view5:{ flexDirection:"row",
-    // alignItems:"center",
-    // backgroundColor:"white",
-    // paddingVertical:25,
-    // justifyContent:"space-between",
-    // marginHorizontal:15,
-    // borderBottomColor:colors.grey4,
-    // borderBottomWidth:1,
-    // flex:1
-    // },
+    view5:{ flexDirection:"row",
+    alignItems:"center",
+    backgroundColor:"white",
+    paddingVertical:25,
+    justifyContent:"space-between",
+    marginHorizontal:15,
+    borderBottomColor:colors.grey4,
+    borderBottomWidth:1,
+    flex:1
+    },
     
-    // view6:{
+    view6:{
     
     
-    // alignItems:"center",
-    // flex:5,
-    // flexDirection:"row"
-    // },
-    // view7:{
-    // backgroundColor:colors.grey6,
-    // height:40,
-    // width:40,
-    // borderRadius:20,
-    // alignItems:"center",
-    // justifyContent:"center",
-    // marginRight:20
+    alignItems:"center",
+    flex:5,
+    flexDirection:"row"
+    },
+    view7:{
+    backgroundColor:colors.grey6,
+    height:40,
+    width:40,
+    borderRadius:20,
+    alignItems:"center",
+    justifyContent:"center",
+    marginRight:20
     
-    // },
+    },
     
-    // map:{
+    map:{
        
-    // height: 150,
-    //  marginVertical: 0,
-    //  width:SCREEN_WIDTH*0.92
-    // },
+    height: 150,
+     marginVertical: 0,
+     width:SCREEN_WIDTH*0.92
+    },
     
-    // text4:{ fontSize:20,
-    //       color:colors.black,
-    //       marginLeft:20,
-    //       marginBottom:20
-    //     },
+    text4:{ fontSize:20,
+          color:colors.black,
+          marginLeft:20,
+          marginBottom:20
+        },
     
     icon1:  {marginLeft:10,
            marginTop:5
@@ -196,21 +270,21 @@ const styles = StyleSheet.create({
     
     }, 
     
-    // location: {
-    //   width: 16,
-    //   height: 16,
-    //   borderRadius:8,
-    //   backgroundColor:colors.blue,
-    //   alignItems:"center",
-    //   justifyContent:"center"
+    location: {
+      width: 16,
+      height: 16,
+      borderRadius:8,
+      backgroundColor:colors.blue,
+      alignItems:"center",
+      justifyContent:"center"
       
-    //   }, 
+      }, 
       
-    // view9:{width:4,
-    // height:4,
-    // borderRadius:2,
-    // backgroundColor:"white"
-    // }
+    view9:{width:4,
+    height:4,
+    borderRadius:2,
+    backgroundColor:"white"
+    }
 
 
 
